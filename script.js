de25e67cc7da5654e619e226256bb29f2b6ef996
Sean D Ward <119538683+Sean-Ward-The-Albino@users.noreply.github.com>
2023-10-03 07:14:36 -0700

    opps gtammar

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function typeSlogan() {
     const sloganElement = document.getElementById('message-content');
-    const sloganText = "hYou're a shinging beacon of joy in my life and I want to just remind you that you have the ability to pursue anything you desire and that I wholheartedly believe in you. You have such a profound and bold personality that I think the world generally lacks and needs now more than ever. This distinct feature about you will help you glow in the future";
+    const sloganText = "You're a shinging beacon of joy in my life and I want to just remind you that you have the ability to pursue anything you desire and that I wholheartedly believe in you. You have such a profound and bold personality that I think the world generally lacks and needs now more than ever. This distinct feature about you will help you glow in the future";
     let charIndex = 0;
 
     function typeNextCharacter() {
